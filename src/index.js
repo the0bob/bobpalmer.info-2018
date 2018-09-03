7af8b119a00604dc676ca23b5a62e2a282dc9e8a
the0bob <bob@bobpalmer.info>
2018-09-03 10:08:03 -0500

    add burger menu

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
-import { configureAnchors } from 'react-scrollable-anchor'
 // import ReactBootstrap from 'react-bootstrap';
-import './styles.css';
+import { scaleRotate as Menu } from 'react-burger-menu';
+import { configureAnchors } from 'react-scrollable-anchor'
+import registerServiceWorker from './registerServiceWorker';
+import './styles/default.css';
+import './styles/burger-menu.css';
 import { Spacer } from './BUI';
-// import Menu from './Menu';
 import Header from './Header';
 import About from './About';
 import Skills from './Skills';
@@ -15,20 +16,42 @@ import Footer from './Footer';
 
 configureAnchors({offset: -60, scrollDuration: 200})
 
+class Nav extends React.Component {
+  showSettings (event) {
+    event.preventDefault();
+  }
+
+  render () {
+    return (
+      <Menu right pageWrapId="page-wrap" outerContainerId="app">
+        <a href="#top">Home</a>
+        <a href="#about">About</a>
+        <a href="#skills">Skills</a>
+        <a href="#portfolio">Portfolio</a>
+        <a href="#contact">Contact</a>
+        <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+      </Menu>
+    );
+  }
+}
+
 class App extends React.Component {
   render() {
     return (
-      <div className="app">
-        <Header />
-        <About />
-        <Spacer />
-        <Skills />
-        <Spacer />
-        <Portfolio />
-        <Spacer />
-        <Contact />
-        <Spacer />
-        <Footer />
+      <div className="app" id="app">
+        <Nav />
+        <main id="page-wrap">
+          <Header />
+          <About />
+          <Spacer />
+          <Skills />
+          <Spacer />
+          <Portfolio />
+          <Spacer />
+          <Contact />
+          <Spacer />
+          <Footer />
+        </main>
       </div>
     );
   }

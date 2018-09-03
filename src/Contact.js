36c5919b75a769c724e791509cebffe91dd81ef9
the0bob <bob@bobpalmer.info>
2018-09-03 03:48:11 -0500

    contact form is working

--- src/Contact.js
+++ src/Contact.js
@@ -1,53 +1,92 @@
 import React from 'react';
+import axios from 'axios';
 import ScrollableAnchor from 'react-scrollable-anchor';
 
-function ContactForm() {
-  return (
-    <div className="col-lg-6 col-md-8 col-sm-10 mx-auto text-center">
-      <h2>Contact Me</h2>
-      <hr className="small" />
-      <form className="mx-auto text-center">
-        <div className="row">
-          <div className="col-md-5">
-            <p>
-              <input type="text" placeholder="Name" required />
-            </p><p>
-              <input type="email" placeholder="Email" required />
-            </p><p>
-              <input type="phone" placeholder="Phone (optional)" />
-            </p>
-          </div>
-          <div className="col-md-7">
-            <textarea cols="40" rows="5" placeholder="What's on your mind?" required></textarea>
-          </div>
-          <div className="clearfix"></div>
-          <div className="col-md-12 text-right">
-            <input type="submit" value="Submit" />
-          </div>
-        </div>
-      </form>
-    </div>
-  );
-}
+export default class Contact extends React.Component {
+  constructor() {
+    super();
+    this.submitForm = this.submitForm.bind(this);
+    this.ContactForm = this.ContactForm.bind(this);
+    this.state = {
+      formSubmitted: false,
+      formDisabled: false,
+      formData: {},
+    };
+  }
+  async submitForm(event) {
+    this.setState({...this.state, formDisabled: true});
+    this.forceUpdate();
+    event.preventDefault();
+    const data = new FormData(event.target);
+    const formData = {
+      name: data.get('name'),
+      email: data.get('email'),
+      phone: data.get('phone'),
+      comments: data.get('comments'),
+    };
+    const response = await axios({
+      url : 'https://hooks.zapier.com/hooks/catch/2516557/8m2iei/',
+      method: 'POST',
+      data: formData,
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+    });
+    this.setState({...this.state, formSubmitted: true});
+    this.forceUpdate();
+    return response;
+  }
 
-function ThankYou() {
-  return (
-    <div className="col-lg-6 col-md-8 col-sm-10 mx-auto text-center">
-      <h2>Thank You</h2>
-      <p>Your message has been sent!</p>
-    </div>
-  );
-}
-export default function() {
-  return (
-    <ScrollableAnchor id="contact">
-    <section className="contact">
-      <div className="container">
-        <div className="row">
-          <ContactForm />
+  ContactForm(props) {
+    if(!props.formSubmitted) {
+      return (
+        <div className="col-lg-6 col-md-8 col-sm-10 mx-auto text-center">
+          <h2>Contact Me</h2>
+          <hr className="small" />
+          <form className="mx-auto text-center" onSubmit={this.submitForm}>
+            <div className="row">
+              <div className="col-md-5">
+                <p>
+                  <input type="text" name="name" placeholder="Name" required/>
+                </p><p>
+                  <input type="email" name="email" placeholder="Email" required/>
+                </p><p>
+                  <input type="phone" name="phone" placeholder="Phone (optional)"/>
+                </p>
+              </div>
+              <div className="col-md-7">
+                <textarea name="comments" cols="40" rows="5" placeholder="What's on your mind?" required></textarea>
+              </div>
+              <div className="clearfix"></div>
+              <div className="col-md-12 text-right">
+                <input type="submit" value="Submit" disabled={this.formDisabled} />
+              </div>
+            </div>
+          </form>
         </div>
-      </div>
-    </section>
-    </ScrollableAnchor>
-  );
+      );
+    } else {
+      return (
+        <div className="col-lg-6 col-md-8 col-sm-10 mx-auto text-center">
+          <h2>Thank You!</h2>
+          <hr className="small" />
+          <p>
+            Your message has been sent!
+          </p>
+        </div>
+      );
+    }
+  }
+
+  render() {
+    return (
+      <ScrollableAnchor id="contact">
+        <section className="contact">
+          <div className="container">
+            <div className="row">
+              <this.ContactForm formSubmitted={this.state.formSubmitted} />
+            </div>
+          </div>
+        </section>
+      </ScrollableAnchor>
+    );
+  }
 }
